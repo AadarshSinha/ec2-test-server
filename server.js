f213bc166150b9ceba8daf6e09da4d58779fc1a9
Prashant <prashantamoli2621@gmail.com>
2023-02-19 21:42:17 +0530

    Working Server

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json())
+
+app.all('/*', async (req, res) => {
+    try {
+        console.log("req came")
+        res.send({message: "Server is successfully deployed now" })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+const PORT = process.env.port || 80;
+
+app.listen(PORT, ()=> {
+    console.log("Server in successfully Running on port ", PORT);
+})
